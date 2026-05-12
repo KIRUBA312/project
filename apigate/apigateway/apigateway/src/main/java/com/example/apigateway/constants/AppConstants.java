153e7efb05e60e20a7bb292a53bcc41518ffc07b
Kirubakaran S <43411753+KIRUBA312@users.noreply.github.com>
2026-05-12 16:23:26 +0530

    rename

--- apigate/apigateway/apigateway/src/main/java/com/example/apigateway/constants/AppConstants.java
+++
@@ -1,6 +0,0 @@
-package com.example.apigateway.constants;
-
-public class AppConstants {
-
-	public static final String API_KEY ="my-secret-key";
-}
--- apigate/apigateway/apigateway/src/main/java/com/example/apigateway/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.apigateway.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.apigateway.entity.Products;
-
-public interface ProductRepository extends JpaRepository<Products, Long>{
-
-}
